a412819c719c500738aef1ce20fe31b77fc39e54
overkoalafied1 <34033601+overkoalafied1@users.noreply.github.com>
2019-05-31 21:25:06 -0400

    1
    1

---
+++ 201905312115/addons/plugin.video.HelixHosting/resources/modules/user.py
@@ -0,0 +1,9 @@
+import base64 as b
+
+id   = b.b64decode('cGx1Z2luLnZpZGVvLkhlbGl4SG9zdGluZw==')
+
+name = b.b64decode('W0NPTE9SbGltZV1IZWxpeFsvQ09MT1JdIFtDT0xPUndoaXRlXUhvc3RpbmdbL0NPTE9SXQ==')
+
+host = b.b64decode('aHR0cDovL2ZmZmZmLnNwYWNl')
+
+port = b.b64decode('ODM=')
